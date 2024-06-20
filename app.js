6076bbe608e60d42c956e4ae05601f06c0818fb7
Dilek Baykara <baykara.d@gmail.com>
2024-06-19 20:03:10 -0400

    changed styling

--- app.js
+++ app.js
@@ -1,174 +1,104 @@
-const nstElements = [
-  "nst-upload-div",
-  "content-upload",
-  "file-info-1",
-  "contentImage",
-  "style-upload",
-  "file-info-2",
-  "styleImage",
-  "style",
-  ".images-div",
-  "upload-title",
-  ".uploaded-images",
-  ".image-div",
-  ".result",
-  "img-placeholder-style",
-  "img-placeholder-content",
-  "stylized-title",
-  "img-style",
-];
+document.getElementById("image1").addEventListener("change", function (event) {
+  const file = event.target.files[0];
+  const reader = new FileReader();
+  const img = document.getElementById("img-placeholder-1");
+  const loaderDiv = document.getElementById("loader-placeholder-1");
+  const loader = document.getElementById("loader-1");
 
-const saElements = [
-  "imageForm",
-  "att-upload-div",
-
-  "image1-btn",
-  "image1",
-  "button-2",
-  "image2-btn",
-  "image2",
-  "compare",
-  "upload-title",
-];
-
-document.getElementById("sa-button").onclick = function () {
-  nstElements.forEach((id) => {
-    let element = document.getElementById(id);
-    if (element) {
-      element.style.display = "none";
-    }
-  });
-
-  saElements.forEach((id) => {
-    let element = document.getElementById(id);
-    if (element) {
-      element.style.display = "flex";
-    }
-  });
-};
-
-document.getElementById("nst-button").onclick = function () {
-  saElements.forEach((id) => {
-    let element = document.getElementById(id);
-    if (element) {
-      element.style.display = "none";
-    }
-    nstElements.forEach((id) => {
-      let element = document.getElementById(id);
-      if (element) {
-        element.style.display = "";
-      }
-    });
-  });
-};
-
-document
-  .getElementById("contentImage")
-  .addEventListener("change", function (event) {
-    const file = event.target.files[0];
-    const reader = new FileReader();
-    const img = document.getElementById("img-placeholder-content");
-    const loaderDiv = document.getElementById("loader-placeholder-content");
-    const loader = document.getElementById("content-loader");
-
-    reader.onload = function (e) {
-      loaderDiv.style.display = "flex";
-      loader.style.display = "block";
-      setTimeout(function () {
-        loaderDiv.style.display = "none";
-        loader.style.display = "none";
-        document.getElementById("uploadedContentImage").src = e.target.result;
-      }, 3000);
-
-      img.style.display = "none";
-    };
-    reader.readAsDataURL(file);
-  });
-
-document
-  .getElementById("styleImage")
-  .addEventListener("change", function (event) {
-    const file = event.target.files[0];
-    const reader = new FileReader();
-    const img = document.getElementById("img-placeholder-style");
-    const loaderDiv = document.getElementById("loader-placeholder-style");
-    const loader = document.getElementById("style-loader");
-
-    reader.onload = function (e) {
-      loaderDiv.style.display = "flex";
-      loader.style.display = "block";
-      setTimeout(function () {
-        loaderDiv.style.display = "none";
-        loader.style.display = "none";
-        document.getElementById("uploadedStyleImage").src = e.target.result;
-      }, 3000);
-
-      img.style.display = "none";
-    };
-    reader.readAsDataURL(file);
-  });
-
-document.getElementById("style").addEventListener("click", (e) => {
-  const img = document.getElementById("img-style");
-  const loaderDiv = document.getElementById("loader-placeholder-result");
-  const loader = document.getElementById("style-loader-3");
-  img.style.display = "none";
-  loaderDiv.style.display = "flex";
-  loader.style.display = "block";
-
-  document
-    .getElementById("imageForm")
-    .addEventListener("submit", async function (event) {
-      event.preventDefault();
-
-      const contentImage = document.getElementById("contentImage").files[0];
-      const styleImage = document.getElementById("styleImage").files[0];
-
-      if (!contentImage || !styleImage) {
-        alert("Please select both images");
-        return;
-      }
-
-      const formData = new FormData();
-      formData.append("content_image", contentImage);
-      formData.append("style_image", styleImage);
-
-      try {
-        const response = await fetch("{END_POINT}/stylize", {
-          method: "POST",
-          body: formData,
-        });
-
-        if (response.ok) {
-          const blob = await response.blob();
-          const url = URL.createObjectURL(blob);
-          document.getElementById("img-style").style.display = "none";
-          document.getElementById("stylizedImage").src = url;
-          loaderDiv.style.display = "none";
-          loader.style.display = "none";
-        } else {
-          alert("Failed to stylize image");
-        }
-      } catch (error) {
-        console.error("Error:", error);
-        alert("An error occurred while processing the images");
-      }
-    });
+  reader.onload = function (e) {
+    loaderDiv.style.display = "flex";
+    loader.style.display = "block";
+    setTimeout(function () {
+      loaderDiv.style.display = "none";
+      loader.style.display = "none";
+      document.getElementById("originalImage1").src = e.target.result;
+    }, 3000);
+    img.style.display = "none";
+  };
+  reader.readAsDataURL(file);
 });
 
-// function loaderTimeOut() {
-//   return new Promise((resolve) => {
-//     setTimeout(() => {
-//       // Other things to do before completion of the promise
-//       loaderDiv.style.display = "none";
-//       loader.style.display = "none";
-//       // The fulfillment value of the promise
-//       console.log("loader worked");
-//     }, 2000);
-//   });
-// }
+document.getElementById("image2").addEventListener("change", function (event) {
+  const file = event.target.files[0];
+  const reader = new FileReader();
+  const img = document.getElementById("img-placeholder-2");
+  const loaderDiv = document.getElementById("loader-placeholder-2");
+  const loader = document.getElementById("loader-2");
 
-// setTimeout(function () {
-//   loaderDiv.style.display = "none";
-//   loader.style.display = "none";
-// }, 3000);
+  reader.onload = function (e) {
+    loaderDiv.style.display = "flex";
+    loader.style.display = "block";
+    setTimeout(function () {
+      loaderDiv.style.display = "none";
+      loader.style.display = "none";
+      document.getElementById("originalImage2").src = e.target.result;
+    }, 3000);
+    img.style.display = "none";
+  };
+  reader.readAsDataURL(file);
+});
+
+document
+  .getElementById("imageForm")
+  .addEventListener("submit", async function (event) {
+    const img = document.getElementById("img-placeholder-3");
+    const loaderDiv = document.getElementById("loader-placeholder-3");
+    const loader = document.getElementById("loader-3");
+
+    const img2 = document.getElementById("img-placeholder-4");
+    const loaderDiv2 = document.getElementById("loader-placeholder-4");
+    const loader2 = document.getElementById("loader-4");
+
+    img.style.display = "none";
+    loaderDiv.style.display = "flex";
+    loader.style.display = "block";
+
+    img2.style.display = "none";
+    loaderDiv2.style.display = "flex";
+    loader2.style.display = "block";
+
+    event.preventDefault();
+
+    const image1 = document.getElementById("image1").files[0];
+    const image2 = document.getElementById("image2").files[0];
+
+    if (!image1 || !image2) {
+      alert("Please select both images to compare their styles.");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("image1", image1);
+    formData.append("image2", image2);
+
+    try {
+      const response = await fetch("{END_POINT}/attribution", {
+        method: "POST",
+        body: formData,
+      });
+
+      if (response.ok) {
+        loaderDiv.style.display = "none";
+        loader.style.display = "none";
+        loaderDiv2.style.display = "none";
+        loader2.style.display = "none";
+
+        const data = await response.json();
+        document.getElementById("styleImage1").src =
+          "data:image/png;base64," + data.image1_features;
+        document.getElementById("styleImage2").src =
+          "data:image/png;base64," + data.image2_features;
+        document.getElementById(
+          "styleSimilarity"
+        ).textContent = `Style Similarity: ${data.style_similarity.toFixed(
+          2
+        )}%`;
+      } else {
+        const error = await response.json();
+        alert("Failed to compare images: " + error.message);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      alert("An error occurred while processing the images");
+    }
+  });
